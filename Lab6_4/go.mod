7668897ca8164fdb86e63279ea14b26b364e59d6
superboss555 <andrei.lyutoev2017@yandex.ru>
2024-10-14 18:51:35 +0400

    lab 6_4

---
+++ Lab6_4/go.mod
@@ -0,0 +1,3 @@
+module lab6_4
+
+go 1.23.1
---
+++ Lab6_4/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	f "fmt"
+	s "sync"
+)
+
+var counter int = 0
+
+func main() {
+	channel := make(chan bool)
+	var mutex s.Mutex
+	for i := 1; i < 10; i++ {
+		go increment(i, channel, &mutex)
+	}
+
+	for i := 1; i < 10; i++ {
+		<-channel
+	}
+
+	f.Println("End")
+}
+
+func increment(number int, ch chan bool, mutex *s.Mutex) {
+	mutex.Lock()
+	for k := 1; k <= 10; k++ {
+		counter++
+		f.Println("Goroutine ", number, "-", counter)
+	}
+
+	mutex.Unlock()
+	ch <- true
+}
